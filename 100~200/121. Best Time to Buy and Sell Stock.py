5eaa48fd080b3c631ccbbd38581d3ec5104a8c8d
vbover <vbover@163.com>
2017-06-19 11:20:14 +0800

    Array Dynamic Programming

---
+++ 100~200/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        result=0
+        if not prices:
+            return result
+        min_price=prices[0]
+        for price in prices:
+            min_price=min(min_price,price)
+            result=max(price-min_price,result)
+        return result
