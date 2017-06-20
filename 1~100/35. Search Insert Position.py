aa55a19196672f66a0ea44472c3fdf3bbe824aca
vbover <vbover@163.com>
2017-06-20 19:08:17 +0800

    Array Binary Search

---
+++ 1~100/35. Search Insert Position.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        begin = 0
+        end = len(nums) - 1
+        while begin <= end:
+            mid = (begin + end) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                end = mid - 1
+            else:
+                begin = mid + 1
+        return begin
