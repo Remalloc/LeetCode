4540c6eb38eb5e93da78ad60929c8a076bd696bb
vbover <vbover@163.com>
2017-09-29 22:51:50 +0800

    array

---
+++ 100~200/162.Find Peak Element.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lens = len(nums)
+        if lens == 1:
+            return 0
+        if lens == 2:
+            return 0 if nums[0] > nums[1] else 1
+        if nums[0] > nums [1]:
+            return 0
+        if nums[lens - 1] > nums[lens - 2]:
+            return lens - 1
+        for mid in range(1, lens - 1):
+            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                return mid
